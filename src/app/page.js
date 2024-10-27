fd838a24d73960b9887a116a4fd6d118c89f4a4a
Abhiram Pai <abhirampai1999@gmail.com>
2024-10-27 09:11:51 +0530

    Move header component to layout and cleanup code

--- src/app/page.js
+++ src/app/page.js
@@ -1,12 +1,5 @@
-
 import BlogPreviews from "./page/BlogPreviews";
-import Header from "./page/Header";
 
 export default function Home() {
-  return (
-    <>
-      <Header />
-      <BlogPreviews />
-    </>
-  );
+  return <BlogPreviews />;
 }
