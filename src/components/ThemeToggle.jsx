2614448aab4b96c295c6953ab8b33008f13b7540
Abhiram Pai <abhirampai1999@gmail.com>
2024-10-28 19:59:23 +0530

    Fix theme toggle icon

--- src/components/ThemeToggle.jsx
+++ src/components/ThemeToggle.jsx
@@ -3,11 +3,23 @@
 import { useTheme } from "next-themes";
 import { Button } from "./ui/button";
 import { MoonIcon, SunIcon } from "@radix-ui/react-icons";
+import { useEffect, useState } from "react";
+import { Skeleton } from "./ui/skeleton";
 
 export const ThemeToggle = () => {
+  const [mounted, setMounted] = useState(false);
   const { theme, setTheme } = useTheme();
 
-  return theme === "dark" ? (
+  useEffect(() => {
+    setMounted(true)
+    setTheme(document.documentElement.classList.contains("dark") ? "dark" : "light");
+  }, [setTheme]);
+
+  if (!mounted) {
+    return <Skeleton className="w-6 h-6 rounded-md" />;
+  }
+
+  return theme==="dark" ? (
     <Button onClick={() => setTheme("light")} variant="ghost">
       <MoonIcon />
     </Button>
