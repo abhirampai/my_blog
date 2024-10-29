12bc1babe79464fd42c074535e10eed25c4c52cb
Abhiram Pai <abhirampai1999@gmail.com>
2024-10-29 07:14:04 +0530

    Added ability to switch themes from the command selector

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -11,10 +11,18 @@ import {
 } from "@/components/ui/command";
 import { blogs } from "@/app/page/constants";
 import Link from "next/link";
-import { FileIcon } from "@radix-ui/react-icons";
+import { FileIcon, SunIcon, MoonIcon, HomeIcon } from "@radix-ui/react-icons";
 import { sortBlogsByPublishedDates } from "@/lib/utils";
+import { useTheme } from "next-themes";
 
 export const SearchBar = ({ open, setOpen }) => {
+  const { setTheme } = useTheme();
+  const themes = {
+    light: <SunIcon />,
+    dark: <MoonIcon />,
+    system: <HomeIcon />,
+  };
+
   useEffect(() => {
     const down = (event) => {
       if (event.key === "k" && (event.metaKey || event.ctrlKey)) {
@@ -34,12 +42,32 @@ export const SearchBar = ({ open, setOpen }) => {
         <CommandGroup heading="Blogs">
           {sortBlogsByPublishedDates(blogs).map((blog) => (
             <CommandItem key={blog.name}>
-              <Link href={`/${blog.slug}`} onClick={() => setOpen(false)} className="w-full">
+              <Link
+                href={`/${blog.slug}`}
+                onClick={() => setOpen(false)}
+                className="w-full"
+              >
                 <div className="flex items-center gap-2">
                   <FileIcon />
                   <span>{blog.name}</span>
                 </div>
               </Link>
+            </CommandItem>
+          ))}
+        </CommandGroup>
+        <CommandGroup heading="Themes">
+          {Object.entries(themes).map(([theme, icon]) => (
+            <CommandItem key={theme}>
+              <div
+                className="flex items-center gap-2 w-full cursor-pointer"
+                onClick={() => {
+                  setTheme(theme);
+                  setOpen(false);
+                }}
+              >
+                {icon}
+                <span>{theme}</span>
+              </div>
             </CommandItem>
           ))}
         </CommandGroup>
