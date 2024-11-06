c4fefe889c080934182622266b6cb18b54497883
Abhiram Pai <abhirampai1999@gmail.com>
2024-11-06 10:52:29 +0530

    Remove thumbnails from constants

--- src/app/page/constants.js
+++ src/app/page/constants.js
@@ -6,7 +6,6 @@ export const blogs = [
       "In my free time, I wanted to learn to create something that would benefit not only my productivity but also help in my work so I decided to create a gem that can list all deprecated methods in a ruby on rails application. And that is how I approached ChatGPT to create a gem...",
     slug: "how_chatgpt_helped_me_to_build_a_ruby_gem",
     keywords: ["chatgpt", "ruby", "rails", "gem", "unused-methods"],
-    thumbnail: "/assets/thumbnails/rails.jpg",
   },
   {
     name: "Going ballistic with Rails upgrade",
@@ -15,7 +14,6 @@ export const blogs = [
       "The journey of moving from Rails 4.2 to Rails 5 was nothing short of a rollercoaster ride. This blog is all about what were the steps taken in order to achieve the changes faster and effectively so that you won’t have to face the challenges in future rails upgrades...",
     slug: "going_ballistic_with_rails_upgrade",
     keywords: ["rails upgrade", "rails5", "rails4.2", "rails-bump"],
-    thumbnail: "/assets/thumbnails/rails.jpg",
   },
   {
     name: "Using Stimulus with Rails engine",
@@ -24,7 +22,6 @@ export const blogs = [
       "During my exploration with ruby on rails, I stumbled upon rails engine and how we can use it to encapsulate or make a ruby on rails application modular by splitting some functionality into smaller manageable sizes. So to learn more about rails engine I started off with creating a mountable rails engine...",
     slug: "using_stimulus_with_rails_engine",
     keywords: ["rails", "rails-engine", "stimulus", "mountable"],
-    thumbnail: "/assets/thumbnails/rails.jpg",
   },
   {
     name: "Integrating Swagger with Rails and Stimulus",
@@ -33,7 +30,6 @@ export const blogs = [
       "Swagger is an api documentation UI that helps users that don’t require access to the application directly but require access to only apis can use. In many of the frameworks for example fastapi has swagger capabilities in build in it...",
     slug: "integrating_swagger_with_rails_and_stimulus",
     keywords: ["swagger", "rails", "stimulus"],
-    thumbnail: "/assets/thumbnails/swagger.png",
   },
   {
     name: "Integrating Scalar with Stimulus",
@@ -42,7 +38,6 @@ export const blogs = [
       "In the previous blog we saw how we can generate swagger.yaml and use it with swagger ui package. In this blog we will be looking into integrating the same swagger.yaml with Scalar. Scalar has a few features of which the one I liked the most is it shows how we can use the api with different clients like node.js, curl, etc...",
     slug: "integrating_scalar_with_stimulus",
     keywords: ["scalar", "stimulus", "rails"],
-    thumbnail: "/assets/thumbnails/scalar.jpg",
   },
   {
     name: "Enhance your Rails app with class-level callbacks",
@@ -51,7 +46,6 @@ export const blogs = [
       "In this blog, we will be looking into how we can enhance our rails app with class-level callbacks. Class-level callbacks are callbacks that are defined on the class level and not on the instance level. This is a great way to add callbacks to your rails app...",
     slug: "how_to_use_callback_directly_on_the_class",
     keywords: ["rails", "class-level callbacks"],
-    thumbnail: "/assets/thumbnails/rails.jpg",
   },
   {
     name: "Using WebLlm to run AI models in browser",
@@ -68,7 +62,6 @@ export const blogs = [
       "Artificial Intelligence",
       "Machine Learning",
     ],
-    thumbnail: "/assets/thumbnails/webllm.jpeg",
   },
   {
     name: "Create simple chat application using Github Models",
@@ -84,12 +77,11 @@ export const blogs = [
       "Machine Learning",
       "React",
     ],
-    thumbnail: "/assets/thumbnails/github.webp",
   },
   {
     name: "BitTorrent from Scratch part-0",
     summary:
-    "This is the first part of a series on implementing Bittorrent from scratch. In this post, we will be looking into bencoding which is how data is communicated between peers and trackers...",
+      "This is the first part of a series on implementing Bittorrent from scratch. In this post, we will be looking into bencoding which is how data is communicated between peers and trackers...",
     publishedDate: "Nov 04, 2024",
     slug: "lets_implement_bittorrent_from_scratch_part_0",
     keywords: [
@@ -100,7 +92,6 @@ export const blogs = [
       "File Sharing",
       "Pseudocode",
     ],
-    thumbnail: "/assets/thumbnails/bittorrent.jpeg",
   },
   {
     name: "BitTorrent from Scratch part-1",
@@ -109,18 +100,17 @@ export const blogs = [
     publishedDate: "Nov 06, 2024",
     slug: "lets_implement_bittorrent_from_scratch_part_1",
     keywords: [
-     "BitTorrent",
-     "torrent file",
-     "metainfo file",
-     "tracker",
-     "tracker get request",
-     "query parameters",
-     "peers, interval",
-     "pseudocode",
-     "file distribution"
+      "BitTorrent",
+      "torrent file",
+      "metainfo file",
+      "tracker",
+      "tracker get request",
+      "query parameters",
+      "peers, interval",
+      "pseudocode",
+      "file distribution",
     ],
-    thumbnail: "/assets/thumbnails/bittorrent.jpeg",
-  }
+  },
 ];
 
 export const IMAGE_BLUR_DATA_URL =
