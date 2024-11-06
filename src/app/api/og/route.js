49d5c96b02db44c2de2f4417ed171d2967e368e8
Abhiram Pai <abhirampai1999@gmail.com>
2024-11-06 10:51:31 +0530

    Use vercel og to generate dynamic og images

---
+++ src/app/api/og/route.js
@@ -0,0 +1,69 @@
+import { ImageResponse } from "@vercel/og";
+
+export const config = {
+  runtime: "edge",
+};
+
+export const GET = (req) => {
+  const { searchParams } = new URL(req.url);
+  const title = searchParams.get("title") || "Default Title";
+  const keywords = searchParams.get("keywords") || ["Default Keywords"];
+
+  return new ImageResponse(
+    (
+      <div
+        style={{
+          display: "flex",
+          flexDirection: "column",
+          justifyContent: "center",
+          alignItems: "center",
+          height: "100%",
+          width: "100%",
+          color: "black",
+          overflowY: "auto",
+          padding: "0 4rem",
+          fontFamily:
+            "ui-monospace, SFMono-Regular, Menlo, Monaco, Consolas, 'Liberation Mono', 'Courier New', monospace",
+          background:
+            "radial-gradient(circle, rgba(238,174,202,1) 0%, rgba(148,187,233,1) 100%)",
+        }}
+      >
+        <div
+          style={{
+            display: "flex",
+            flexDirection: "column",
+            justifyContent: "center",
+          }}
+        >
+          <h2>{title}</h2>
+          <div
+            style={{
+              display: "flex",
+              gap: "0.5rem",
+              width: "50%",
+              flexWrap: "wrap",
+            }}
+          >
+            {keywords.split(",").map((keyword, index) => (
+              <div
+                key={index}
+                style={{
+                  marginBottom: "1rem",
+                  border: "1px solid white",
+                  borderRadius: "0.5rem",
+                  padding: "0.5rem",
+                }}
+              >
+                {keyword}
+              </div>
+            ))}
+          </div>
+        </div>
+      </div>
+    ),
+    {
+      width: 1200,
+      height: 630,
+    },
+  );
+};
